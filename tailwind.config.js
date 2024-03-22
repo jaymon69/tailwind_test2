5245acc0cab6376bcb80268f55aa331deb250532
Jay Mon <kyawgii035@gmail.com>
2024-03-22 11:59:50 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+      sans: ['The Girl Next Door', 'cursive'],
+    },
+   colors:{
+    padauk: '#1b2438' ,
+    kyaynee: 'red',
+   }
+  },},
+    
+  plugins: [],
+}
